da0bdd3b2dcbd91f4098dcea1646c5baabb86616
mostafa <mostafaameen203030@gmail.com>
2025-06-25 19:39:39 +0300

    create add showmodelbottomsheet

--- lib/view/note_view.dart
+++ lib/view/note_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/view/widgets/noteviewbody.dart';
-
 class NoteView extends StatelessWidget {
   const NoteView({super.key});
 
@@ -8,8 +7,51 @@ class NoteView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body:noteviewbody(),
-      floatingActionButton: FloatingActionButton(onPressed: (){},child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+
+        showModalBottomSheet(context: context, builder: (context){
+          return addnotebuttomsheet();
+        });
+      },child: Icon(Icons.add),),
     );
   }
 }
 
+
+class addnotebuttomsheet extends StatelessWidget {
+  const addnotebuttomsheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8)
+                ),
+              hintText: 'Title',
+              hintStyle:TextStyle(color: Colors.cyanAccent)
+            ),
+          ),
+          SizedBox(height: 20,),
+          TextFormField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+
+                    borderRadius: BorderRadius.circular(8)
+                ),
+                hintText: 'Content',
+                hintStyle:TextStyle(color: Colors.cyanAccent)
+            ),
+          ),
+          SizedBox(height: 50,),
+
+          ElevatedButton(onPressed: (){}, child: Text('Add Note'),style: ElevatedButton.styleFrom(backgroundColor: Colors.cyanAccent,foregroundColor: Colors.black),)
+        ],
+      ),
+    );
+  }
+}
