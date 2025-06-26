959876c4a14903f7e921979cce76c6d96a07bc89
mostafa <mostafaameen203030@gmail.com>
2025-06-26 19:01:22 +0300

    Create add note cubits

---
+++ lib/cubits/addnote/add_note_cubit.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'add_note_state.dart';
+
+class NoteCubit extends Cubit<NoteState> {
+  NoteCubit() : super(NoteInitial());
+}
